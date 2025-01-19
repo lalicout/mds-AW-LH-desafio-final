b6e8d71e255ebbd50d0f304d9ac4b00fbd5d580f
Lais Coutinho <laismcs89@gmail.com>
2025-01-19 12:36:54 -0300

    Fix stg_person_address model

--- dbt/adventure_works_transform/models/staging/person/stg_person_address.sql
+++ dbt/adventure_works_transform/models/staging/person/stg_person_address.sql
@@ -3,23 +3,24 @@
 with address_raw as (
     -- fonte de dados bruta da tabela 'address'
     select * 
-    from {{ source('raw_aw_person', 'address') }}
+    from {{ source('RAW_AW_ADDRESS', 'ADDRESS')}}
 ),
 
 address_transformation as (
     select
-        addressid as address_id,
-        ,initcap(addressline1) as addressline1,
-        ,initcap(addressline2) as addressline2,
-        ,initcap(city) as city,
-        ,stateprovinceid as id_state_province,
-        ,postalcode as postal_code,
-        ,spatiallocation as spatial_location
-        ,rowguid as rowguide
-        ,modifieddate as last_updated_at
+
+        addressid as address_id
+        , initcap(addressline1) as addressline1
+        , initcap(addressline2) as addressline2
+        , initcap(city) as city
+        , stateprovinceid as id_state_province
+        , coalesce(postalcode, 'UNKNOWN') as postal_code
+        , spatiallocation as spatial_location
+        , rowguid as rowguide
+        , modifieddate as last_updated_at
 
     from address_raw
 )
 
 select * 
-from address_transformation;
+from address_transformation
